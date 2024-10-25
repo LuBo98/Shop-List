30fdb5bfbfea36ca68f67c06c019acec2ef0f4ff
lukas <lukas0bornscheuer@gmail.com>
2024-10-26 00:24:08 +0200

    init

--- init.sql
+++ init.sql
@@ -1,5 +1,11 @@
+-- Ensure the database exists
+CREATE DATABASE IF NOT EXISTS `shop-list`;
+
+-- Use the `shop-list` database
+USE `shop-list`;
+-- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS items_to_get (
                                             id INT AUTO_INCREMENT PRIMARY KEY,
-                                            name VARCHAR(255),
-    amount VARCHAR(255)
+                                            name VARCHAR(255) NOT NULL,
+    amount VARCHAR(255) NOT NULL
     );
